70275d2192dcf850a413f28b1d2408d6710088bd
ramisalah2002 <ramisalah2002@gmail.com>
2023-05-08 10:24:55 +0100

    test

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,24 +1,31 @@
-import logo from './logo.svg';
+import React from 'react';
+import { BrowserRouter as Router, Routes, Route, Link } from 'react-router-dom';
+import test1 from './test1';
+import test2 from './test2';
 import './App.css';
+
+const routes = [
+  { path: '/test1', component: test1 },
+  { path: '/test2', component: test2 },
+];
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Router>
+      <div className="container">
+        <div className="sidebar">
+          <Link className='sidebar-link' to="/test1">Go to Test 1</Link>
+          <Link className='sidebar-link' to="/test2">Go to Test 2</Link>
+        </div>
+        <div className="main">
+          <Routes>
+            {routes.map(({ path, component: Component }) => (
+              <Route key={path} path={path} element={<Component />} />
+            ))}
+          </Routes>
+        </div>
+      </div>
+    </Router>
   );
 }
 
