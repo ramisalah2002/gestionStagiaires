a5ec42cebe72f8c61e7c54ad95ef8d6af5039b39
ramisalah2002 <ramisalah2002@gmail.com>
2023-05-12 11:37:26 +0100

    adding components file

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,5 +1,6 @@
 import React from 'react';
-import Sidebar from './Sidebar';
+import Sidebar from '../components/Sidebar/Sidebar';
+import Header from '../components/Header/Header';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { faSearch,faCircleUser, faCalendarDays } from '@fortawesome/free-solid-svg-icons';
 import './App.css';
@@ -7,26 +8,9 @@ import './App.css';
 function App() {
   return (
     <div className="app">
-      <Sidebar />
+      <Sidebar/>
       <main className="main-content">
-          <div className='header'>
-            <div className='admin-container'>
-              <FontAwesomeIcon className='admin-icon' icon={faCircleUser} />
-              <div className='admin-info'>
-                <label className='admin-name'>Nom Administrateur</label>
-                <label className='admin-post'>Poste administrateur</label>
-              </div>
-              <div className='vertical-line'></div>
-              <div className='today-container'>
-                <FontAwesomeIcon className='calendar-icon' icon={faCalendarDays} />
-                <label className='today-label'>23 fév</label>
-              </div>
-            </div>
-            <div className='search-container'>
-              <FontAwesomeIcon className='search-icon' icon={faSearch} />
-              <input className='search-input' placeholder='Rechercher ...' type="text"/>
-            </div>
-          </div>
+          <Header/>
           <div className='sections-container'>
             <div className='stagiaires-container'>
               
