09a481d3bd8a1718670d92da1f6e6e16ca37d14a
Boulaajoul-Anass <boulaajoul.anass@gmail.com>
2023-05-14 14:50:45 +0100

    loginPage with some errors

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 import React from "react";
 import Sidebar from "../components/Sidebar/Sidebar";
 import Header from "../components/Header/Header";
@@ -9,36 +8,11 @@ import {
   faCalendarDays,
 } from "@fortawesome/free-solid-svg-icons";
 import "./App.css";
+import Homepage from "../pages/Homepage/Homepage";
+import Loginpage from "../pages/LoginPage/LoginPage.jsx";
 
 function App() {
-  return (
-    <div className="app">
-      <Sidebar />
-      <main className="main-content">
-        <Header />
-        <div className="sections-container">
-          <div className="stagiaires-container"></div>
-          <div className="project-abscence-section">
-            <div className="project-container"></div>
-            <div className="abscence-container"></div>
-          </div>
-        </div>
-      </main>
-    </div>
-=======
-import React from 'react';
-import Sidebar from '../components/Sidebar/Sidebar';
-import Header from '../components/Header/Header';
-import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
-import { faSearch,faCircleUser, faCalendarDays } from '@fortawesome/free-solid-svg-icons';
-import './App.css';
-import Homepage from '../pages/Homepage/Homepage';
-
-function App() {
-  return (
-    <Homepage/>
->>>>>>> bdbad0eff1d6ea9029630eac243215b86e3e2f52
-  );
+  return <Loginpage />;
 }
 
 export default App;
