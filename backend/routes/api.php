83cc80617b2bbdc916c0fb4c22a2920ade9cd312
Boulaajoul-Anass <boulaajoul.anass@gmail.com>
2023-06-05 22:38:37 +0100

    backing front

--- backend/routes/api.php
+++ backend/routes/api.php
@@ -52,6 +52,29 @@ Route::get('/images', [ImageController::class, 'index']);
 
 
 
+//Absence
+Route::apiResource('absence',\App\Http\Controllers\AbsenceController::class);
+
+//Attestation
+Route::apiResource('attestation',\App\Http\Controllers\AttestationController::class);
+
+//Avancement
+Route::apiResource('avancement',\App\Http\Controllers\AvancementController::class);
+
+//Equipe
+Route::apiResource('equipe',\App\Http\Controllers\EquipeController::class);
+
+//Etablissment
+Route::apiResource('etablissement',\App\Http\Controllers\EtablissementController::class);
+
+//Mdp_token
+Route::apiResource('mdp_token',\App\Http\Controllers\Mdp_tokensController::class);
+
+//Message
+Route::apiResource('message',\App\Http\Controllers\MessageController::class);
+
+//participation
+Route::apiResource('participation',\App\Http\Controllers\ParticipationController::class);
 
 //Presentation
 Route::apiResource('presentation',\App\Http\Controllers\PresentationController::class);
@@ -59,8 +82,8 @@ Route::apiResource('presentation',\App\Http\Controllers\PresentationController::
 //Projet
 Route::apiResource('projet',\App\Http\Controllers\ProjetController::class);
 
-//Rapport
-Route::apiResource('rapport',\App\Http\Controllers\RapportController::class);
+//RapportStage
+Route::apiResource('rapportStage',\App\Http\Controllers\RapportStageController::class);
 
 //Reunion
 Route::apiResource('reunion',\App\Http\Controllers\ReunionController::class);
@@ -71,11 +94,17 @@ Route::apiResource('stage',\App\Http\Controllers\StageController::class);
 //Stagiaire
 Route::apiResource('stagiaire',\App\Http\Controllers\StagiaireController::class);
 
+//Technologie
+Route::apiResource('technologie',\App\Http\Controllers\TechnologieController::class);
+
 //User
 Route::apiResource('user',\App\Http\Controllers\UserController::class);
 
 //Utilisateur
 Route::apiResource('utilisateur',\App\Http\Controllers\UtilisateurController::class);
+
+//UtilisationTechnologie
+Route::apiResource('utilisationTechnologie',\App\Http\Controllers\UtilisationTechnologieController::class);
 
 //UseFull Requests
 
